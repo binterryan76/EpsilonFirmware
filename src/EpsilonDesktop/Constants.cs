c0f535172793b7edf9f46375244b65d32e001579
binterryan76 <91400196+binterryan76@users.noreply.github.com>
2026-08-25 20:43:06 -0500

    Fixed AppData directory.

--- src/EpsilonDesktop/Constants.cs
+++ src/EpsilonDesktop/Constants.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.VisualBasic.FileIO;
-using System.IO;
+﻿using System.IO;
 
 namespace EpsilonDesktop;
 
 public static class Constants
 {
     public const string APP_NAME = "Epsilon Desktop";
-    public static string APP_DATA_DIRECTORY { get; } = Path.Combine(SpecialDirectories.CurrentUserApplicationData, "EpsilonDesktop");
+    public static string APP_DATA_DIRECTORY { get; } = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+        "EpsilonDesktop");
+
     public static string SETTINGS_FILE_PATH { get; } = Path.Combine(APP_DATA_DIRECTORY, "settings.json");
 
 }
