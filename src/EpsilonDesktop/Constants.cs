d768b8d9ad38aaecc5568e07691b6e9a4e5d3601
binterryan76 <91400196+binterryan76@users.noreply.github.com>
2026-08-29 17:11:45 -0500

    Made arrow text constants

--- src/EpsilonDesktop/Constants.cs
+++ src/EpsilonDesktop/Constants.cs
@@ -11,4 +11,33 @@ public static class Constants
 
     public static string SETTINGS_FILE_PATH { get; } = Path.Combine(APP_DATA_DIRECTORY, "settings.json");
 
+    /// <summary>
+    /// U+1F818 Heavy Leftwards Arrow with Equilateral Arrowhead
+    /// </summary>
+    public const string ARROW_LEFT = "\uD83E\uDC18";
+
+    /// <summary>
+    /// U+1F81A Heavy Rightwards Arrow with Equilateral Arrowhead
+    /// </summary>
+    public const string ARROW_RIGHT = "\uD83E\uDC1A";
+
+    /// <summary>
+    /// U+1F81B Heavy Downwards Arrow with Equilateral Arrowhead
+    /// </summary>
+    public const string ARROW_DOWN = "\uD83E\uDC1B";
+
+    /// <summary>
+    /// U+1F819 Heavy Upwards Arrow with Equilateral Arrowhead
+    /// </summary>
+    public const string ARROW_UP = "\uD83E\uDC19";
+
+    /// <summary>
+    /// U+2B6E Clockwise Triangle-Headed Open Circle Arrow
+    /// </summary>
+    public const string ARROW_CLOCKWISE = "\u2B6E";
+
+    /// <summary>
+    /// U+21B6 Anticlockwise Top Semicircle Arrow
+    /// </summary>
+    public const string ARROW_COUNTERCLOCKWISE = "\u21B6";
 }
